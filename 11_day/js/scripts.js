74c34d0427c8191ebfba4d18f48fce943cb615ea
Andru <roobotik@gmail.com>
2018-02-02 22:38:24 +0200

    add end day 11 plair done

--- 11_day/js/scripts.js
+++ 11_day/js/scripts.js
@@ -3,9 +3,9 @@ const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
-const togle = player.querySelectorAll('.toggle');
+const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelectorAll('.playrt__slider');
+const ranges = player.querySelectorAll('.player__slider');
 
 /*build out functions*/
 function togglePlay() {
@@ -15,9 +15,65 @@ function togglePlay() {
     //     video.play();
     // } else {
     //     video.pause();
-    // }
+    // } 
+}
+
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    console.log(icon);
+    toggle.textContent = icon;
+}
+
+// функцыя для пропуска нескольких секунд
+
+function skip() {
+     console.log('Skipping!');
+     console.log(this.dataset.skip);
+     video.currentTime += parseFloat(this.dataset.skip);
+}
+
+// регулеровка громкости и скорости
+
+function handleRanngeUpdate(){
+    console.log(this.value);
+    video[this.name] = this.value;
+    console.log(this.value);
+}
+
+function handleProgress() {
+    const percent = (video.currentTime/video.duration)*100;
+    progressBar.style.flexBasis = `${percent}%`;
 }
 
 
-
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+    console.log(e);
+}
 /*Hook up the event listners*/
+
+// КНОПКИ ПУЗЫ И ПЛЕЯ + ОБНОВЛЕНИЕ
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
+ 
+// пРОПУСК СЕКУНД
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+// Ползунки громкости и скорости
+ranges.forEach(range => range.addEventListener('change', handleRanngeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRanngeUpdate));
+
+
+video.addEventListener('timeupdate', handleProgress);
+
+let mousedown = false;
+
+progress.addEventListener('mousemove', () => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+
+
+progress.addEventListener('click', scrub);
