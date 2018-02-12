772d80c67414096fe18c5e1bdc8b2c5dc84d86a8
Andru Ischenko <roobotik@gmail.com>
2018-02-12 17:48:28 +0200

    ad

--- 15_day_server/app/routes/note_routes.js
+++ 15_day_server/app/routes/note_routes.js
@@ -1,16 +1,36 @@
 module.exports = function(app, db) {
   app.post('/notes', (req, res) => {
     const note = { text: req.body.body, title: req.body.title };
-    
+    console.log(note);
+    res.send('Hello')
    
-   
-    db.collection('notes').insert(note, (err, result) => {
-      if (err) { 
-        res.send({ 'error': 'An error has occurred' }); 
-      } else {
-        res.send(result.ops[0]);
-      }
-    });
+    var noteColection = db.collection('inventory').insertMany([ 
+      // MongoDB adds the _id field with an ObjectId if _id is not present
+      { item: "journal", qty: 25, status: "A",
+          size: { h: 14, w: 21, uom: "cm" }, tags: [ "blank", "red" ] },
+      { item: "notebook", qty: 50, status: "A",
+          size: { h: 8.5, w: 11, uom: "in" }, tags: [ "red", "blank" ] },
+      { item: "paper", qty: 100, status: "D",
+          size: { h: 8.5, w: 11, uom: "in" }, tags: [ "red", "blank", "plain" ] },
+      { item: "planner", qty: 75, status: "D",
+          size: { h: 22.85, w: 30, uom: "cm" }, tags: [ "blank", "red" ] },
+      { item: "postcard", qty: 45, status: "A",
+          size: { h: 10, w: 15.25, uom: "cm" }, tags: [ "blue" ] }
+   ])
+   .then(function(result) {
+
+   })
+
+
+ 
+
+    // db.collection('notes').insert(note, (err, result) => {
+    //   if (err) { 
+    //     res.send({ 'error': 'An error has occurred' });  
+    //   } else {
+    //     res.send(result.ops[0]);
+    //   }
+    // });
   });
 }
 
--- 15_day_server/server.js
+++ 15_day_server/server.js
@@ -1,25 +1,55 @@
-// server.js
-const express        = require('express');
-const MongoClient    = require('mongodb').MongoClient;
-const bodyParser     = require('body-parser');
-const db             = require('./config/db');
-const app            = express();
+// const express        = require('express');
+// const MongoClient    = require('mongodb').MongoClient;
+// const bodyParser     = require('body-parser');
+// const db             = require('./config/db');
+// const app            = express();
+// const port = 8000;
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// MongoClient.connect(db.url, (err, client) => {
+//   if (err) return console.log(err)
+//   console.log(client);
+
+//  const db = client.db('click_button');
+//  console.log(db);
+
+//   require('./app/routes')(app, client);
+//   app.listen(port, () => {
+//     console.log('We are live on ' + port);
+//   });
+//   app.get('/', (req, res) => {
+//     res.send('hello ApI');
+//   });               
+// })
+
+const express = require('express');
 const port = 8000;
-app.use(bodyParser.urlencoded({ extended: true }));
+const app = express();
 
-MongoClient.connect(db.url, (err, database) => {
-  if (err) return console.log(err)
-
- const db = database.db('click_button');
-
-  require('./app/routes')(app, database);
   app.listen(port, () => {
     console.log('We are live on ' + port);
   });
-  app.get('/', (req, res) => {
-    res.send('hello ApI');
-  });               
-})
 
+// app.get('/', function (req, res) {
+//   res.send('hallo API');
+// })
 
+// app.get('/artists', function (req, res) {
+//   res.send(artists);
+// })
 
+// var artists = [
+//   {
+//     id: 1,
+//     name: 'Iron man'
+//   },
+//   {
+//     id: 2,
+//     name: 'Cool man'
+//   },
+//   {
+//     id: 3,
+//     name: 'Superman'
+//   },
+
+// ]
