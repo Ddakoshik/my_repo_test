846957eaf78aac462610cc708f96bb05d803eacb
Andrii Ishchenko <aishchenko@deloitte.com>
2018-10-08 17:59:45 +0300

    arr function curry

---
+++ 17_curry-function/main.js
@@ -0,0 +1,33 @@
+// ***************************************
+// Карирование - функцыональное програмирование 
+// Для передачи функции в кари, будет ждать заданого количества аргумет=нтов а потом выполнятся
+  
+  var curry =  function (fn) {
+      var arity = fn.length
+    console.log('arity', arity);
+    
+    return function f1(...args) {
+        console.log('f1 args', args)
+      if (args.length >= arity) {
+          console.log('enough arguments')
+        return fn(...args)
+      } else {
+          console.log('need more arguments')
+        return function f2(...moreArgs) {
+            console.log('f2', moreArgs)
+          var newArgs = args.concat(moreArgs)
+          return f1(...newArgs)
+        }
+      }
+    }
+  }
+  
+
+  var add = function (a, b ,c) {
+      return a + b + c;
+  }
+  var curriedAdd = curry(add);
+  
+  var result = curriedAdd(1)(1, 1)
+  
+  console.log('result', result)
